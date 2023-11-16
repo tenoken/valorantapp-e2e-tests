4267c5893e481f8f4df06b7ab8df8b1d404f543f
tenoken <shiroiparadina@gmail.com>
2023-11-16 19:26:24 -0300

    first commit

---
+++ src/test/java/endtoend/navigation/DuckDuckGoHomePage.java
@@ -0,0 +1,8 @@
+package endtoend.navigation;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("https://duckduckgo.com")
+public class DuckDuckGoHomePage extends PageObject {
+}
